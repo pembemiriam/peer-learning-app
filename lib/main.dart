126563637d01db6901feb023383db1fb90bd6e04
Ngenge Senior <seniorngenge@gmail.com>
2019-12-06 14:51:29 +0100

    Added gender stats

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './Login/signup.dart';
+import 'gender_stats.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,11 +8,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gender Stats',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Signup(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Gender Stats"),
+
+        ),
+
+        body: Center(child: GenderStats()),
+      ),
     );
   }
 }
