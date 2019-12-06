9ea403221f596fcec8bfe7bc79c07fead07dbdfd
fonkamloic <fonkamloic@gmail.com>
2019-12-06 19:36:34 +0100

    signin_page

--- lib/Login/signin.dart
+++ lib/Login/signin.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:peer_learning/dashboard.dart';
+import 'package:peer_learning/Login/signup.dart';
 
 class SigninPage extends StatefulWidget {
   static String tag = 'login';
@@ -19,35 +20,33 @@ class _SigninPageState extends State<SigninPage> {
   bool _autoValidate = false;
   bool _loadingVisible = false;
 
-
-
   @override
   Widget build(BuildContext context) {
     final Map args = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.blue,
-        title: Text(
-          'Sign In',
-          style: TextStyle(fontSize: 30.0),
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.blue,
+          title: Text(
+            'Sign In',
+            style: TextStyle(fontSize: 30.0),
+          ),
         ),
-      ),
-      body: Center(
+        body: Center(
           child: Container(
             alignment: Alignment.center,
             child: Form(
-            key: _formKey,
-            autovalidate: _autoValidate,
-            child: ListView(
-              children: <Widget>[
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
+              key: _formKey,
+              autovalidate: _autoValidate,
+              child: ListView(
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
 //                    Center(
 //                      child: Container(
 //                        margin: EdgeInsets.only(left: 16, top: 8),
@@ -66,65 +65,83 @@ class _SigninPageState extends State<SigninPage> {
 //                            fontSize: 18, fontWeight: FontWeight.normal),
 //                      ),
 //                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 22, right: 22, top: 16, bottom: 8),
-                      child: TextFormField(
-                        autofocus: false,
-                        controller: _email,
+                      Padding(
+                        padding: EdgeInsets.only(
+                            left: 22, right: 22, top: 16, bottom: 8),
+                        child: TextFormField(
+                          autofocus: false,
+                          controller: _email,
 //                        validator: Validator.validateEmail,
-                        keyboardType: TextInputType.emailAddress,
-                        style: TextStyle(fontSize: 18),
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 22, right: 22, top: 8, bottom: 8),
-                      child: TextFormField(
-                        autofocus: false,
-                        controller: _password,
-//                        validator: Validator.validatePassword,
-                        obscureText: true,
-                        style: TextStyle(fontSize: 18),
-                        keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                        ),
-                      ),
-                    ),
-
-                    Padding(
-                      padding: EdgeInsets.only(left: 22, right: 22, top: 16, bottom: 8),
-                      child: Container(
-                        color: Colors.transparent,
-                        width: MediaQuery.of(context).size.width,
-                        height: 50,
-                        child: FlatButton(
-                          shape: new RoundedRectangleBorder(
-//                          borderRadius: new BorderRadius.circular(30.0),
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(fontSize: 18),
+                          decoration: InputDecoration(
+                            labelText: 'Email',
                           ),
-                          onPressed: () {},
-                          color: Colors.blue,
-                          child: Text(
-                            "Sign In",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: 'Raleway',
-                              fontSize: 22.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            left: 22, right: 22, top: 8, bottom: 8),
+                        child: TextFormField(
+                          autofocus: false,
+                          controller: _password,
+//                        validator: Validator.validatePassword,
+                          obscureText: true,
+                          style: TextStyle(fontSize: 18),
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.only(
+                            left: 22, right: 22, top: 16, bottom: 8),
+                        child: Container(
+                          color: Colors.transparent,
+                          width: MediaQuery.of(context).size.width,
+                          height: 50,
+                          child: FlatButton(
+                            shape: new RoundedRectangleBorder(
+//                          borderRadius: new BorderRadius.circular(30.0),
+                                ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Dashboard()));
+                            },
+                            color: Colors.blue,
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Raleway',
+                                fontSize: 22.0,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ],
+
+                      Text("Create an account:"),
+                      FlatButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignUpPage()));
+                        },
+                        child: Text("sign up"),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-          ),
-        )
-    );
+        ));
   }
 
   Future<void> _changeLoadingVisible() async {
@@ -132,5 +149,4 @@ class _SigninPageState extends State<SigninPage> {
       _loadingVisible = !_loadingVisible;
     });
   }
-
 }
