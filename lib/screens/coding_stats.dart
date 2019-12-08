fc71d948698227f7c6916baaced3b2fb70f8e736
fonkamloic <fonkamloic@gmail.com>
2019-12-08 22:31:37 +0100

    this show stats about users skills in various PL's

--- lib/screens/coding_stats.dart
+++ lib/screens/coding_stats.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart';
+import 'package:peer_learning/components/statsGraph.dart';
 
-class CodingStats extends StatefulWidget{
-
+class CodingStats extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return CodingStatsState();
   }
-
 }
 
-class CodingStatsState extends State<CodingStats>{
-
+class CodingStatsState extends State<CodingStats> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Coding Statistics',
-          style: TextStyle(fontSize: 30),),
+        title: Text(
+          'Coding Statistics',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  height: 400,
+                  width: double.infinity,
+                  child: DonutAutoLabelChart.withSampleData()),
+            ],
+          ),
+        ),
       ),
     );
   }
