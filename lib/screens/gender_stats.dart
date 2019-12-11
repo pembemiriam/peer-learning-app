3892383fa8ce36be1b3a39acc1214dbb54ce90af
fonkamloic <fonkamloic@gmail.com>
2019-12-11 05:23:27 +0100

    screen_rotation_support

--- lib/screens/gender_stats.dart
+++ lib/screens/gender_stats.dart
@@ -8,46 +8,52 @@ class GenderStats extends StatefulWidget {
 class _GenderStatsState extends State<GenderStats> {
   @override
   Widget build(BuildContext context) {
+    final _width = MediaQuery.of(context).size.width;
+    final _height = MediaQuery.of(context).size.height;
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: Text(
             'Gender Statistics',
-            style: TextStyle(fontSize: 30),
+            style: TextStyle(fontSize: 20),
           ),
         ),
         body: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Center(
-            child: Column(
+            child: ListView(
               children: <Widget>[
-                Image.asset(
-                  'assets/boy.png',
-                  width: 100.0,
-                  height: 100.0,
+                Column(
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/boy.png',
+                      width: 100.0,
+                      height: 100.0,
+                    ),
+                    SizedBox(
+                      height: _width < _height ? _width / 20 : _height / 20,
+                    ),
+                    Text(
+                      '223 males',
+                      style: TextStyle(fontSize: 25.0),
+                    ),
+                    SizedBox(
+                      height: _width < _height ? _width / 20 : _height / 20,
+                    ),
+                    Image.asset(
+                      'assets/girl.png',
+                      width: 100.0,
+                      height: 100.0,
+                    ),
+                    SizedBox(
+                      height: _width < _height ? _width / 20 : _height / 20,
+                    ),
+                    Text(
+                      '223 females',
+                      style: TextStyle(fontSize: 25.0),
+                    )
+                  ],
                 ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Text(
-                  '223 males',
-                  style: TextStyle(fontSize: 25.0),
-                ),
-                SizedBox(
-                  height: 46.0,
-                ),
-                Image.asset(
-                  'assets/girl.png',
-                  width: 100.0,
-                  height: 100.0,
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Text(
-                  '223 females',
-                  style: TextStyle(fontSize: 25.0),
-                )
               ],
             ),
           ),
