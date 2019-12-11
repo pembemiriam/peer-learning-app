fdbe8c42c6d3eec8a40045e5e380c56074113947
fonkamloic <fonkamloic@gmail.com>
2019-12-11 05:20:39 +0100

    update to the mentor card

--- lib/components/mentorProfileCard.dart
+++ lib/components/mentorProfileCard.dart
@@ -1,7 +1,15 @@
 import 'package:card_selector/card_selector.dart';
 import 'package:flutter/material.dart';
 
-const list = ["Miriam", "Loic", "3", "4", "5"];
+const list = ["Amanda", "Miriam", "Loic", "Senior", "Tabi"];
+const lang = ["Dart", "Python", 'Javascript', "Kotlin", "Java"];
+const img = [
+  'assets/images/amanda.jpeg',
+  'assets/images/miriam.jpeg',
+  'assets/images/Loic.jpg',
+  'assets/images/senior.jpeg',
+  'assets/images/Loic.jpg',
+];
 
 const colors = [
   Colors.blue,
@@ -28,11 +36,24 @@ class MentorProfileCard extends StatelessWidget {
           children: <Widget>[
             CircleAvatar(
               radius: _width < _height ? _width / 5 : _height / 5,
-              backgroundImage: AssetImage('assets/images/Loic.jpg'),
+              backgroundImage: AssetImage(img[i]),
             ),
-            Text(
-              list[i],
-              style: Theme.of(context).textTheme.title,
+            Expanded(
+//              height: 100,
+//              width: 80,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    list[i],
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(lang[i], style: Theme.of(context).textTheme.subtitle),
+                ],
+              ),
             ),
           ],
         ),
